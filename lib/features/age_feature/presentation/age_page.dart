7fef7f45db428bfac9048ed357e676fc87863c83
SarvagyaRamBhandari <sarvagya.bhandari@acme-software.com>
2022-01-12 16:26:49 +0545

    some changes to routes and ui

--- lib/features/age_feature/presentation/age_page.dart
+++ lib/features/age_feature/presentation/age_page.dart
@@ -1,6 +1,4 @@
-import 'package:clean_code_training/features/age_feature/model/age_viewmodel.dart';
 
-import 'package:clean_framework/clean_framework_providers.dart';
 import 'package:flutter/material.dart';
 
 class AgePage extends StatelessWidget {
@@ -8,28 +6,45 @@ class AgePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return AgeUI();
-  }
-}
-
-class AgeUI extends UI<AgeViewModel> {
-  @override
-  Widget build(BuildContext context, viewModel) {
     return Scaffold(
       body: Column(
-        children: const [
-          Text('Enter Your Name'),
-          TextField(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children:
+        [
+          const Text('Enter Your Name'),
+
+          const TextField(
             // onChanged: viewModel.name,
           ),
+
+      ElevatedButton( onPressed: () {  },
+      child: const Text("calculate age"))
         ],
       ),
     );
   }
-
-  @override
-  Presenter<ViewModel, Output, UseCase<Entity>> create(PresenterBuilder<AgeViewModel> builder) {
-    // TODO: implement create
-    throw UnimplementedError();
-  }
 }
+//
+// class AgeUI extends UI<AgeViewModel> {
+//
+//
+//   @override
+//   Widget build(BuildContext context, viewModel) {
+//     return Scaffold(
+//       body: Column(
+//         children: const [
+//           Text('Enter Your Name'),
+//           TextField(
+//             // onChanged: viewModel.name,
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+//
+//   // @override
+//   // Presenter<ViewModel, Output, UseCase<Entity>> create(PresenterBuilder<AgeViewModel> builder) {
+//   //   // TODO: implement create
+//   //   throw UnimplementedError();
+//   }
+
