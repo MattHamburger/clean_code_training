2426022eebefb360bdc0986c04081ef6e76e6324
SarvagyaRamBhandari <sarvagya.bhandari@acme-software.com>
2022-01-13 21:40:37 +0545

    local

--- lib/providers.dart
+++ lib/providers.dart
@@ -1,5 +1,5 @@
 import 'package:clean_framework/clean_framework.dart';
-import 'package:clean_framework/clean_framework_providers.dart';
+
 
 ProvidersContext _providersContext = ProvidersContext();
 
